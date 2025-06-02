5067ce91c08267a6680a3f90b370e47e486282bc
Joy <122730271+joyinalgorithms@users.noreply.github.com>
2025-06-02 13:58:34 +0800

    Fix app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config["SESSION PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.run(host="0.0.0.0", port=port)
+
 db = SQL("sqlite:///tourism.db")
 
 
